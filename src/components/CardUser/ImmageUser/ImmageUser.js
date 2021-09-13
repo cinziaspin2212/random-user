b3788696177dae7c572248665207dfb2d51d5cb0
cmascalchidema <c.mascalchi@dema-engineering.it>
2021-09-13 12:46:15 +0200

    converted class component ImmageUser to a functional component

--- src/components/CardUser/ImmageUser/ImmageUser.js
+++ src/components/CardUser/ImmageUser/ImmageUser.js
@@ -1,15 +1,18 @@
-import { Component } from 'react';
-import './ImmageUser.css';
-class ImmageUser extends Component {
-    render() {
-        console.log('image',this.props.infoUserDet.immage)
-        return (<div className='image-container'>
-            <div className='image-user'><img src={this.props.infoUserDet.immage}  /></div>
-        </div>)
-    }
+import { Component } from "react";
+import "./ImmageUser.css";
+// class ImmageUser extends Component
+const ImmageUser = (props) => {
+  // render() {
+  console.log("image", props.infoUserDet.immage);
+  return (
+    <div className="image-container">
+      <div className="image-user">
+        <img src={props.infoUserDet.immage} />
+      </div>
+    </div>
+  );
+};
 
-    
-}
-
+// }
 
 export default ImmageUser;
