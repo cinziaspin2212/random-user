902354330fd571526d041d402b16f5714097161d
cmascalchidema <c.mascalchi@dema-engineering.it>
2021-09-13 12:54:27 +0200

    converted class component PanelUser to a functional component

--- src/components/CardUser/PanelUser/PanelUser.js
+++ src/components/CardUser/PanelUser/PanelUser.js
@@ -10,73 +10,74 @@ import {
   faLock,
 } from "@fortawesome/free-solid-svg-icons";
 
-class PanelUser extends Component {
-  render() {
-    console.log("props: ", this.props);
-    return (
-      <div className="panel-user">
-        <button
-          className="button"
-          id="User"
-          onMouseEnter={(e) => this.props.envenHover(e)}
-          data-title="User"
-          /* onMouseLeave={(e) => this.props.evenLeave(e)}*/
-        >
-          <FontAwesomeIcon icon={faUser} />
-        </button>
+// class PanelUser extends Component
+const PanelUser = (props) => {
+  // render() {
+  console.log("props: ", props);
+  return (
+    <div className="panel-user">
+      <button
+        className="button"
+        id="User"
+        onMouseEnter={(e) => props.envenHover(e)}
+        data-title="User"
+        /* onMouseLeave={(e) => this.props.evenLeave(e)}*/
+      >
+        <FontAwesomeIcon icon={faUser} />
+      </button>
 
-        <button
-          className="button"
-          id="Email"
-          onMouseEnter={(e) => this.props.envenHover(e)}
-         /* onMouseLeave={(e) => this.props.evenLeave(e)}*/
-          data-title="Email"
-        >
-          <FontAwesomeIcon icon={faEnvelopeSquare} />
-        </button>
+      <button
+        className="button"
+        id="Email"
+        onMouseEnter={(e) => props.envenHover(e)}
+        /* onMouseLeave={(e) => this.props.evenLeave(e)}*/
+        data-title="Email"
+      >
+        <FontAwesomeIcon icon={faEnvelopeSquare} />
+      </button>
 
-        <button
-          className="button"
-          id="Birthday"
-          onMouseEnter={(e) => this.props.envenHover(e)}
-         /* onMouseLeave={(e) => this.props.evenLeave(e)}*/
-          data-title="Birthday"
-        >
-          <FontAwesomeIcon icon={faBirthdayCake} />
-        </button>
+      <button
+        className="button"
+        id="Birthday"
+        onMouseEnter={(e) => props.envenHover(e)}
+        /* onMouseLeave={(e) => this.props.evenLeave(e)}*/
+        data-title="Birthday"
+      >
+        <FontAwesomeIcon icon={faBirthdayCake} />
+      </button>
 
-        <button
-          className="button"
-          id="Address"
-          onMouseEnter={(e) => this.props.envenHover(e)}
-       /*   onMouseLeave={(e) => this.props.evenLeave(e)}*/
-          data-title="Address"
-        >
-          <FontAwesomeIcon icon={faHouseUser} />
-        </button>
+      <button
+        className="button"
+        id="Address"
+        onMouseEnter={(e) => props.envenHover(e)}
+        /*   onMouseLeave={(e) => this.props.evenLeave(e)}*/
+        data-title="Address"
+      >
+        <FontAwesomeIcon icon={faHouseUser} />
+      </button>
 
-        <button
-          className="button"
-          id="Phone"
-          onMouseEnter={(e) => this.props.envenHover(e)}
+      <button
+        className="button"
+        id="Phone"
+        onMouseEnter={(e) => props.envenHover(e)}
         /*  onMouseLeave={(e) => this.props.evenLeave(e)}*/
-          data-title="Phone"
-        >
-          <FontAwesomeIcon icon={faPhoneSquare} />
-        </button>
+        data-title="Phone"
+      >
+        <FontAwesomeIcon icon={faPhoneSquare} />
+      </button>
 
-        <button
-          className="button"
-          id="Password"
-          onMouseEnter={(e) => this.props.envenHover(e)}
-         /* onMouseLeave={(e) => this.props.evenLeave(e)}*/
-          data-title="Password"
-         >
-          <FontAwesomeIcon icon={faLock} />
-        </button>
-      </div>
-    );
-  }
-}
+      <button
+        className="button"
+        id="Password"
+        onMouseEnter={(e) => props.envenHover(e)}
+        /* onMouseLeave={(e) => this.props.evenLeave(e)}*/
+        data-title="Password"
+      >
+        <FontAwesomeIcon icon={faLock} />
+      </button>
+    </div>
+  );
+};
+// }
 
 export default PanelUser;
