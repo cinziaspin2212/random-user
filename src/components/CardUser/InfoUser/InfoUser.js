a8ca562d25af81a492c7f3d863e00327baf68592
cmascalchidema <c.mascalchi@dema-engineering.it>
2021-09-13 12:50:18 +0200

    converted class component InfoUser to a functional component

--- src/components/CardUser/InfoUser/InfoUser.js
+++ src/components/CardUser/InfoUser/InfoUser.js
@@ -1,41 +1,42 @@
 import { Component } from "react";
 import "./InfoUser.css";
 
-class InfoUser extends Component {
-  changeInfoHandler = () => {
+// class InfoUser extends Component
+const InfoUser = (props) => {
+  const changeInfoHandler = () => {
     /* console.log('tt',this.props.infoEvent.whoEvent);*/
-    const whoEv = this.props.infoEvent.whoEvent;
+    const whoEv = props.infoEvent.whoEvent;
     switch (whoEv) {
       case "User":
-        return this.props.infoUserDet.fullName;
+        return props.infoUserDet.fullName;
       case "Email":
-        return this.props.infoUserDet.email;
+        return props.infoUserDet.email;
       case "Birthday":
-        return this.props.infoUserDet.birthday;
+        return props.infoUserDet.birthday;
       case "Address":
-        return this.props.infoUserDet.address;
+        return props.infoUserDet.address;
       case "Phone":
-        return this.props.infoUserDet.telephone;
+        return props.infoUserDet.telephone;
       case "Password":
-        return this.props.infoUserDet.password;
+        return props.infoUserDet.password;
       default:
         return "";
     }
   };
 
-  render() {
-    console.log("infoUserDet-infouser", this.props);
-    return (
-      <div className="info-user">
-        <div className="info-user-descr">
-          <label id="descrizione">{this.props.infoEvent.whoEvent}</label>
-        </div>
-        <div className="info-user-dett">
-          <label id="info">{this.changeInfoHandler()}</label>
-        </div>
+  // render() {
+  console.log("infoUserDet-infouser", props);
+  return (
+    <div className="info-user">
+      <div className="info-user-descr">
+        <label id="descrizione">{props.infoEvent.whoEvent}</label>
       </div>
-    );
-  }
-}
+      <div className="info-user-dett">
+        <label id="info">{changeInfoHandler()}</label>
+      </div>
+    </div>
+  );
+};
+// }
 
 export default InfoUser;
