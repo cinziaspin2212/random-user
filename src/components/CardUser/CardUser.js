a182147442dfa27b75240fdda8134f15a3735d84
cmascalchidema <c.mascalchi@dema-engineering.it>
2021-09-14 10:42:51 +0200

    converted class component CardUser to a functional component

--- src/components/CardUser/CardUser.js
+++ src/components/CardUser/CardUser.js
@@ -3,27 +3,39 @@ import "./CardUser.css";
 import ImmageUser from "./ImmageUser/ImmageUser";
 import InfoUser from "./InfoUser/InfoUser";
 import PanelUser from "./PanelUser/PanelUser";
-class CardUser extends Component {
-  render() {
-    console.log("infoUserDet", this.props);
-    return (
-      <div className="card">
-        <ImmageUser infoUserDet={this.props.infoUserDet} />
-        <InfoUser
+// class CardUser extends Component
+const CardUser = (props) => {
+  // render() {
+  // console.log("infoUserDet", this.props);
+  console.log("infoUserDet", props);
+  return (
+    <div className="card">
+      {/* <ImmageUser infoUserDet={this.props.infoUserDet} /> */}
+      <ImmageUser infoUserDet={props.infoUserDet} />
+      {/* <InfoUser
           infoUserDet={this.props.infoUserDet}
           infoEvent={this.props.infoEvent}
-        />
-        <PanelUser
+        /> */}
+      <InfoUser infoUserDet={props.infoUserDet} infoEvent={props.infoEvent} />
+      {/* <PanelUser
           envenHover={(e) => {
             this.props.envenHover(e);
           }}
           evenLeave={(e) => {
             this.props.evenLeave(e);
           }}
-        />
-      </div>
-    );
-  }
-}
+        /> */}
+      <PanelUser
+        envenHover={(e) => {
+          props.envenHover(e);
+        }}
+        evenLeave={(e) => {
+          props.evenLeave(e);
+        }}
+      />
+    </div>
+  );
+};
+// }
 
 export default CardUser;
