efe161ec82ebe7075449f3968435ac67c1e2c23b
cmascalchidema <c.mascalchi@dema-engineering.it>
2021-09-20 11:49:54 +0200

    converted class component App to a functional component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,65 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Component } from "react";
+import { Component, useState } from "react";
 import CardUser from "./components/CardUser/CardUser";
 import ButtonNext from "./components/ButtonNext/ButtonNext";
 import Container from "./components/Loading/Loading";
 
-class App extends Component {
-  state = {
-    loading: true,
-    userState: {
-      fullName: "",
-      email: "",
-      birthday: "",
-      address: "",
-      telephone: "",
-      password: "",
-      immage: "",
-    },
-    iconPanel: {
-      whoEvent: "",
-    },
-  };
+// class App extends Component
+const App = (props) => {
+  // state = {
+  //   loading: true,
+  //   userState: {
+  //     fullName: "",
+  //     email: "",
+  //     birthday: "",
+  //     address: "",
+  //     telephone: "",
+  //     password: "",
+  //     immage: "",
+  //   },
+  //   iconPanel: {
+  //     whoEvent: "",
+  //   },
+  // };
 
-  mouseOverButtonHandler = (e) => {
+  const [loadingState, setLoadingState] = useState(true);
+
+  const [userState, setUserState] = useState({
+    fullName: "",
+    email: "",
+    birthday: "",
+    address: "",
+    telephone: "",
+    password: "",
+    immage: "",
+  });
+
+  const [iconPanelState, seticonPanelState] = useState({
+    whoEvent: "",
+  });
+
+  const mouseOverButtonHandler = (e) => {
     console.log("over", e.target.id);
     console.log("des", document.getElementById("descrizione").value);
     // e.target.style.background = "cornflowerblue";
 
     const el = e.target.id;
-    this.setState({
-      iconPanel: {
-        whoEvent: el,
-      },
+    seticonPanelState({
+      whoEvent: el,
     });
   };
 
-  mouseLeaveButtonHandler = (e) => {
+  const mouseLeaveButtonHandler = (e) => {
     console.log("leave", e.target.id);
     // e.target.style.background = "gray";
-    this.setState({
-      iconPanel: {
-        whoEvent: "",
-      },
+    seticonPanelState({
+      whoEvent: "",
     });
   };
-  retrieveDataHandler = () => {
-    this.setState({
-      loading:true,
-    });
-    
+
+  const retrieveDataHandler = () => {
+    setLoadingState(true);
+
     fetch("https://randomuser.me/api", {
       method: "GET", // or 'PUT'
       headers: {
@@ -60,59 +71,58 @@ class App extends Component {
         //console.log('Success:', data);
         const userData = data.results[0];
         //console.log("userData:", userData)
-        this.setState({
-          loading: false,
-          userState: {
-            fullName:
-              userData.name.title +
-              " " +
-              userData.name.first +
-              " " +
-              userData.name.last,
-            email: userData.email,
-            birthday: new Date(userData.dob.date).toDateString(),
-            address:
-              userData.location.street.name +
-              " " +
-              userData.location.street.number,
-            telephone: userData.phone,
-            password: userData.login.password,
-            immage: userData.picture.medium,
-          },
-          iconPanel: {
-            whoEvent: "User",
-          },
+        setLoadingState(false);
+        setUserState({
+          fullName:
+            userData.name.title +
+            " " +
+            userData.name.first +
+            " " +
+            userData.name.last,
+          email: userData.email,
+          birthday: new Date(userData.dob.date).toDateString(),
+          address:
+            userData.location.street.name +
+            " " +
+            userData.location.street.number,
+          telephone: userData.phone,
+          password: userData.login.password,
+          immage: userData.picture.medium,
+        });
+        seticonPanelState({
+          whoEvent: "User",
         });
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   };
-  componentDidMount() {
-    this.retrieveDataHandler();
-  }
 
-  render() {
-    console.log("userData:", this.state.userState);
-    return this.state.loading ? (
-      <Container />
-    ) : (
-      <div className="App">
-        <CardUser
-          infoUserDet={this.state.userState}
-          infoEvent={this.state.iconPanel}
-          envenHover={(e) => {
-            this.mouseOverButtonHandler(e);
-          }}
-          evenLeave={(e) => {
-            this.mouseLeaveButtonHandler(e);
-          }}
-        />
-        {/*  passo le info */}
-        <ButtonNext changeUser={this.retrieveDataHandler} />
-        {/* passo la funzione che cambia lo stato */}
-      </div>
-    );
-  }
-}
+  // componentDidMount() {
+  //   this.retrieveDataHandler();
+  // }
+
+  // render() {
+  console.log("userData:", userState);
+  return loadingState ? (
+    <Container />
+  ) : (
+    <div className="App">
+      <CardUser
+        infoUserDet={userState}
+        infoEvent={iconPanelState}
+        envenHover={(e) => {
+          mouseOverButtonHandler(e);
+        }}
+        evenLeave={(e) => {
+          mouseLeaveButtonHandler(e);
+        }}
+      />
+      {/*  passo le info */}
+      <ButtonNext changeUser={retrieveDataHandler} />
+      {/* passo la funzione che cambia lo stato */}
+    </div>
+  );
+};
+// }
 export default App;
